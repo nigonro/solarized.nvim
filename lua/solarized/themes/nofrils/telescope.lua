b840fb8a5b8c713b4bce39a64a63720c49eb5b24
nigonro <nicolas+github@flia.gr>
2023-09-20 12:04:12 +0200

    Improves telescope colors

--- lua/solarized/themes/nofrils/telescope.lua
+++ lua/solarized/themes/nofrils/telescope.lua
@@ -2,13 +2,13 @@ return function(c, config)
 	local utils = require("solarized.utils")
 	local set_hl = utils.set_hl
 
-	set_hl("TelescopeSelection", { fg = c.base2, bg = c.base01 })
+	set_hl("TelescopeSelection", { bg = c.base02 })
 	set_hl("TelescopeSelectionCaret", { link = "TelescopeSelection" })
 	set_hl("TelescopeMultiSelection", { link = "Type" })
 	set_hl("TelescopeMultiIcon", { fg = c.cyan })
 
 	-- "Normal" in the floating windows created by telescope.
-	set_hl("TelescopeNormal", { fg = c.base0, bg = c.base02 }, { transparent = config.transparent })
+	set_hl("TelescopeNormal", { fg = c.base0, bg = c.base03 }, { transparent = config.transparent })
 	set_hl("TelescopePreviewNormal", { link = "TelescopeNormal" })
 	set_hl("TelescopePromptNormal", { link = "TelescopeNormal" })
 	set_hl("TelescopeResultsNormal", { link = "TelescopeNormal" })
@@ -16,7 +16,7 @@ return function(c, config)
 	-- Border highlight groups.
 	--   Use TelescopeBorder to override the default.
 	--   Otherwise set them specifically
-	set_hl("TelescopeBorder", { link = "WinSeparator" })
+	set_hl("TelescopeBorder", { link = "TelescopeNormal" })
 	set_hl("TelescopePromptBorder", { link = "TelescopeBorder" })
 	set_hl("TelescopeResultsBorder", { link = "TelescopeBorder" })
 	set_hl("TelescopePreviewBorder", { link = "TelescopeBorder" })
@@ -37,6 +37,6 @@ return function(c, config)
 	-- Used for the prompt prefix
 	set_hl("TelescopePromptPrefix", { fg = c.magenta })
 
-	set_hl("TelescopePreviewLine", { link = "Visual" })
-	set_hl("TelescopePreviewMatch", { link = "Search" })
+	set_hl("TelescopePreviewMatch", { fg = c.magenta, bg = "NONE", bold = true })
+	set_hl("TelescopePreviewLine", { bg = c.base02, bold = true })
 end
